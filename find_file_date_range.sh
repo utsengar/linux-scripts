5be2e955643f9c08c6cf2adb31b806708e0b8067
Utkarsh Sengar <utkarsh2012@gmail.com>
2010-11-06 15:55:56 -0700

    added comment

--- find_file_date_range.sh
+++ find_file_date_range.sh
@@ -1,5 +1,7 @@
+#create two dummy files with the start and the end date
 touch -t 0907020000 before
 touch -t 0907312359 after
 
+#find all the files between the 2 dummy files and "ls -lat" or "mv"" the result
 find . -newer before \! -newer after -exec ls -lat {} \;
 find . -newer before \! -newer after -exec mv {} temp_backup \;
