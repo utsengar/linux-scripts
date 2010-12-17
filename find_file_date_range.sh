fec01584c4829e2f71d2ee6f82c9188fb7453375
Utkarsh Sengar <utkarsh2012@gmail.com>
2010-12-17 12:18:47 -0800

    added check file size

--- find_file_date_range.sh
+++ find_file_date_range.sh
@@ -5,3 +5,6 @@ touch -t 0907312359 after
 #find all the files between the 2 dummy files and "ls -lat" or "mv"" the result
 find . -newer before \! -newer after -exec ls -lat {} \;
 find . -newer before \! -newer after -exec mv {} temp_backup \;
+
+#get the size of the sub-folders in a folder
+du . -h --max-depth=1
